a0b2a58d8d9dccb3871c65a77099006248fe633a
adamijak <jakub.adamik@greycorbel.com>
2024-03-21 17:28:42 +0100

    feat: init

---
+++ AzureAuth/CertHandler.cs
@@ -0,0 +1,22 @@
+using System.Security.Cryptography.X509Certificates;
+using Azure.Identity;
+
+namespace AzureAuth;
+
+public static class CertHandler
+{
+    public static async Task Handle(string tenantId, string clientId, string certThumbprint, string[] scopes)
+    {
+        var cert = GetCertificate(certThumbprint);
+        var credential = new ClientCertificateCredential(tenantId, clientId, cert);
+        var token = await credential.GetTokenAsync(new(scopes));
+        Console.Write(token.Token);
+    }
+
+    private static X509Certificate2 GetCertificate(string thumbprint)
+    {
+        using var store = new X509Store(StoreName.My, StoreLocation.CurrentUser, OpenFlags.ReadOnly);
+        var certificates = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, false);
+        return certificates.First();
+    }
+}
---
+++ AzureAuth/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.CommandLine;
+using AzureAuth;
+
+var tenantId = new Option<string>(["-t", "--tenant"])
+{
+    IsRequired = true
+};
+var clientId = new Option<string>(["-c", "--client"])
+{
+    IsRequired = true
+};
+var certThumbprint = new Option<string>(["--cert"])
+{
+    IsRequired = true
+};
+var scopes = new Option<string[]>(["--scopes"])
+{
+    AllowMultipleArgumentsPerToken = true,
+    Arity = ArgumentArity.OneOrMore,
+    IsRequired = true,
+};
+
+var rootCmd = new RootCommand();
+var certCmd = new Command("cert", "Certificate")
+{
+    tenantId,
+    clientId,
+    certThumbprint,
+    scopes,
+};
+
+certCmd.SetHandler(CertHandler.Handle, tenantId, clientId, certThumbprint,scopes);
+rootCmd.Add(certCmd);
+
+await rootCmd.InvokeAsync(args);
